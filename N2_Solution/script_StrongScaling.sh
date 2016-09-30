632ef0214c99f4e2bf18f8c35bf2e80569fe598d
MarcoCasty <m.castaldo92@gmail.com>
2016-09-30 18:16:17 +0200

    Create script_StrongScaling.sh

---
+++ N2_Solution/script_StrongScaling.sh
@@ -0,0 +1,23 @@
+
+if [ ! -f generate_output.txt ]
+then
+        echo "file  does not exist.";
+else
+        rm generate_output.txt
+fi
+
+if [ ! -f stderror.csv ]
+then
+        echo "file  does not exist.";
+else
+        rm stderror.csv
+fi
+
+mpicc $1 -o $1.out
+
+for((X=2;X<=16; X*=2))
+do
+mpirun -np $X $1.out $2 8 1 1 g 2>> stderror.csv
+done
+
+exit 0
