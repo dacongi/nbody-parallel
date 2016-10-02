7d4f5b2ebb03eeb4ec1058203db22f073ea57107
MarcoCasty <m.castaldo92@gmail.com>
2016-10-02 16:04:54 +0200

    Update script_StrongScaling.sh

--- N2_Solution/script_StrongScaling.sh
+++ N2_Solution/script_StrongScaling.sh
@@ -1,21 +1,30 @@
 #!/bin/sh
 
-
-if [ ! -f generate_output.txt ]
-then
-        rm generate_output.txt
-fi
-
-if [ ! -f stderrorStrongScaling.csv ]
-then
-        rm stderrorStrongScaling.csv
+if [ -d StrongScaling_CSV ]
+        then
+        rm -rf StrongScaling_CSV
+        mkdir StrongScaling_CSV
+        else
+        mkdir StrongScaling_CSV
 fi
 
 /oxygen/compilers/mpi/openmpi/1.8.2.jdk.1.8/bin/mpicc $1 -o $1.out
 
-for((X=2;X<=16; X=X*2))
+for((Y=50000; Y<=150000; Y+=50000))
 do
-/oxygen/compilers/mpi/openmpi/1.8.2.jdk.1.8/bin/mpirun -np $X -host oxygen9,oxygen10,oxygen11,oxygen12,oxygen13,oxygen14,oxygen15,oxygen16,oxygen17,oxygen19,oxygen20,oxygen21,oxygen22,oxygen23,oxygen24 $1.out $2 10 0.1 1 g 2>> stderrorStrongScaling.csv 
+
+        if [ ! -f generate_output.txt ]
+                then
+                rm generate_output.txt
+        fi
+
+
+        for((X=2;X<=16; X=X*2))
+        do
+        /oxygen/compilers/mpi/openmpi/1.8.2.jdk.1.8/bin/mpirun -np $X -host oxygen9,oxygen10,oxygen11,oxygen12,oxygen13,oxygen14,oxygen15,oxygen16,oxygen17,oxygen19,oxygen20,oxygen21,oxygen22,oxygen23,oxygen24 $1.out $Y 10 0.1 1 g 2>> stderrorStrongScaling_$Y.csv 
+        done
+
+        mv stderrorStrongScaling_$Y.csv StrongScaling_CSV/
 done
 
 exit 0
