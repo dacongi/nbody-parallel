aff550459732fa362a7f8769fe952f6146746e61
MarcoCasty <m.castaldo92@gmail.com>
2016-10-04 19:16:54 +0200

    Update script_StrongScaling.sh

--- NlogN_Solution/script_StrongScaling.sh
+++ NlogN_Solution/script_StrongScaling.sh
@@ -8,7 +8,7 @@ if [ -d StrongScaling_CSV ]
         mkdir StrongScaling_CSV
 fi
 
-/oxygen/compilers/mpi/openmpi/1.8.2.jdk.1.8/bin/mpicc $1 -o $1.out tree.c
+/oxygen/compilers/mpi/openmpi/1.8.2.jdk.1.8/bin/mpicc $1 -o $1.out tree.c -lm
 
 for((Y=50000; Y<=150000; Y+=50000))
 do
